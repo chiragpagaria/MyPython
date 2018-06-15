1241270885ac1adeea2db2abb21d7d9f76e98669
chirag <chirag@127.0.0.1localhost.localdomainlocalhost>
2018-06-15 20:46:02 +0530

    final

--- WordPredictor.py
+++ WordPredictor.py
@@ -6,7 +6,7 @@ couDict = list()
 for line in file:
     for i in line.split():
         word.append(i)
-
+#print(word)
 for i in range(len(word)-1):
     tempFinal = list()
     if word[i] not in tempW:
@@ -29,21 +29,31 @@ for li in final:
         wc[i] = wc.get(i, 0) + 1
     couDict.append(wc)
 
+tkey = list()
+for i in couDict:
+    l = list(i.keys())
+    #print(l)
+    tkey.append(l[0])
+
+for i in range(len(couDict)):
+    del couDict[i][tkey[i]]
+
+#print(couDict)
+
+#print(tkey)
 final = list()
 for i in couDict:
     l = dict()
     for key, value in sorted(i.items(), key=lambda item: (item[1], item[0])):
         l[key] = value
     final.append(l)
-print(final)
+#print(final)
 
-flag = True
 enterText = input("Enter The word")
-for i in final:
-    if(flag):
-        temp = list(i.keys())
-        if enterText == temp[0]:
-            print("Next Word Will Be ",temp[-1])
-            flag = False
-        else:
-            continue
+if enterText in tkey:
+    inx = tkey.index(enterText)
+    #print(tkey[inx],inx)
+    temp = list(final[inx].keys())
+    print(temp[-1])
+else:
+    print("No Suggestion")
